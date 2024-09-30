9489cb857c2d59ce1014a36947028bdfee2c8220
Jess Friedersdorff <jess@friedersdorff.com>
2024-09-30 10:20:07 +0100

    Update slurm_subheader.sh
    updated subheader with comments to better describe the options.

--- slurm_subheader.sh
+++ slurm_subheader.sh
@@ -6,24 +6,25 @@
 #$5 is the partition
 
 if [ "$#" -ne 5 ]; then
-    echo "subheader.sh - a generic script to print out a Slurm submission file. "
+    echo "subheader.sh - a generic script to print out a Slurm submission file." 
+    echo "Use sinfo (with --all or --partition) to find information about the partitions."
     echo "Script usage:"
     echo "subheader.sh <Run Name> <Max Memory in GB> <Number of Threads> <Email/User> <partition>"
     echo ""
     echo "i.e. subheader.sh my_amazing_job 10 8 jess.friedersdorff amd"
 else
     mydir=$(pwd)
-    echo "#!/bin/bash --login"
-    echo "#SBATCH --error=${mydir}/${1}_%J.err"
-    echo "#SBATCH --output=${mydir}/${1}_%J.output"
-    echo "#SBATCH --partition=${5}"
-    echo "#SBATCH --nodes=1"
-    echo "#SBATCH --ntasks=1"
-    echo "#SBATCH --cpus-per-task=${3}"
-    echo "#SBATCH --job-name=${1}"
-    echo "#SBATCH --mail-type=END,FAIL"
-    echo "#SBATCH --mail-user=${4}@email.com"
-    echo "#SBATCH --mem=${2}gb"
+    echo "#!/bin/bash --login"  # Tell slurm how to run the script below - set to bash, can also set to shell or zsh.
+    echo "#SBATCH --error=${mydir}/${1}_%J.err"  # Set name of stderr, will use job id. 
+    echo "#SBATCH --output=${mydir}/${1}_%J.output"  # Set name of stdout, will use job id. 
+    echo "#SBATCH --partition=${5}"  # Select the partition - use sinfo on the CL to find info about this. 
+    echo "#SBATCH --nodes=1"  # Number of nodes
+    echo "#SBATCH --ntasks=1"  # Number of CPUs but not necessairly on the same node - for OMP. 
+    echo "#SBATCH --cpus-per-task=${3}"  # Number of CPUs on same node - for multithreading. 
+    echo "#SBATCH --job-name=${1}"  # Job name - you choose. 
+    echo "#SBATCH --mail-type=END,FAIL"  # Set to email on end and fail. [NONE, BEGIN, END, FAIL, REQUEUE, ALL]
+    echo "#SBATCH --mail-user=${4}@email.com"  # Change this to correct email address.
+    echo "#SBATCH --mem=${2}gb"  # Integer number of gb of memory. 
     #echo "#SBATCH --account=hpcuser"
 
  fi
